90042b55811cbdf2aec1cd43a5e3eccadfe97f15
firebb <yhuo1@andrew.cmu.edu>
2017-05-08 20:32:56 -0400

    baseline establish

--- forward.cpp
+++ forward.cpp
@@ -3,36 +3,30 @@ double forward(int *data, int len, int nstates, int nobvs,
         double *prior, double * trans, double *obvs)
 {
     /* construct trellis */
-   // double alpha[len][nstates];
-   // double beta[len][nstates];
-	double *alpha = (double *)malloc(len * nstates * sizeof(double));
-	double *beta = (double *)malloc(len * nstates * sizeof(double));
+    // double alpha[len][nstates];
+    // double beta[len][nstates];
+    double *alpha = (double *)malloc(len * nstates * sizeof(double));
+    double *beta = (double *)malloc(len * nstates * sizeof(double));
 
     double loglik;
 
-    #pragma omp parallel
-    {
-        #pragma omp for
-        for (int i = 0; i < len; i++) {
-            for (int j = 0; j < nstates; j++) {
-                alpha[i * nstates + j] = - INFINITY;
-                beta[i * nstates + j] = - INFINITY;
-            }
+    for (int i = 0; i < len; i++) {
+        for (int j = 0; j < nstates; j++) {
+            alpha[i * nstates + j] = - INFINITY;
+            beta[i * nstates + j] = - INFINITY;
         }
+    }
 
-        /* forward pass */
-        #pragma omp for
-        for (int i = 0; i < nstates; i++) {
-            alpha[i] = prior[i] + obvs[IDX(i,data[0],nobvs)];
-        }
-        
-        for (int i = 1; i < len; i++) {
-            #pragma omp for 
-            for (int j = 0; j < nstates; j++) {
-                for (int k = 0; k < nstates; k++) {
-                    double p = alpha[(i-1) * nstates + k] + trans[IDX(k,j,nstates)] + obvs[IDX(j,data[i],nobvs)];
-                    alpha[i * nstates + j] = logadd(alpha[i * nstates + j], p);
-                }
+    /* forward pass */
+    for (int i = 0; i < nstates; i++) {
+        alpha[i] = prior[i] + obvs[IDX(i,data[0],nobvs)];
+    }
+
+    for (int i = 1; i < len; i++) {
+        for (int j = 0; j < nstates; j++) {
+            for (int k = 0; k < nstates; k++) {
+                double p = alpha[(i-1) * nstates + k] + trans[IDX(k,j,nstates)] + obvs[IDX(j,data[i],nobvs)];
+                alpha[i * nstates + j] = logadd(alpha[i * nstates + j], p);
             }
         }
     }
