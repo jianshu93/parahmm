cd0390cad961d67d288ccba9f1788e68210f5c56
firebb <yhuo1@andrew.cmu.edu>
2017-05-10 21:23:34 -0400

    unroll

--- baum_welch.cpp
+++ baum_welch.cpp
@@ -13,12 +13,6 @@ float forward_backward(int *data, int len, int nstates, int nobvs, float *prior,
     float *beta = (float *)aligned_alloc(32, len * nstates * sizeof(float));
 
     float loglik;
-    for (int i = 0; i < len; i++) {
-        for (int j = 0; j < nstates; j++) {
-            alpha[i*nstates + j] = - INFINITY;
-            beta[i*nstates + j] = - INFINITY;
-        }
-    }
 
     /* forward pass */
     for (int i = 0; i < nstates; i++) {
@@ -37,18 +31,26 @@ float forward_backward(int *data, int len, int nstates, int nobvs, float *prior,
        //     }
        // }
        
-        for (int j = 0; j < nstates; j+=8) {
+        for (int j = 0; j < nstates; j+=16) {
             __m256 result_AVX = _mm256_set1_ps(-INFINITY);
+            __m256 result_AVX2 = _mm256_set1_ps(-INFINITY);
             __m256 obvs_AVX = _mm256_load_ps(obvs + data[i] * nstates + j);
+            __m256 obvs_AVX2 = _mm256_load_ps(obvs + data[i] * nstates + j + 8);
             for (int k = 0; k < nstates; k++) {
                 __m256 alpha_AVX = _mm256_set1_ps(alpha[(i-1) * nstates + k]);
+                __m256 alpha_AVX2 = _mm256_set1_ps(alpha[(i-1) * nstates + k]);
                 __m256 trans_AVX = _mm256_load_ps(trans + k*nstates + j);
+                __m256 trans_AVX2 = _mm256_load_ps(trans + k*nstates + j + 8);
                 // calculate p
                 alpha_AVX = _mm256_add_ps(alpha_AVX, trans_AVX);
+                alpha_AVX2 = _mm256_add_ps(alpha_AVX2, trans_AVX2);
                 alpha_AVX = _mm256_add_ps(alpha_AVX, obvs_AVX);
+                alpha_AVX2 = _mm256_add_ps(alpha_AVX2, obvs_AVX2);
                 result_AVX = logadd(result_AVX, alpha_AVX);
+                result_AVX2 = logadd(result_AVX2, alpha_AVX2);
             }
             _mm256_store_ps(alpha + i*nstates + j, result_AVX);
+            _mm256_store_ps(alpha + i*nstates + j + 8, result_AVX2);
         }
     }
     printf("Forward taken %.4f milliseconds\n",  (CycleTimer::currentSeconds() - startTime) * 1000);
@@ -91,38 +93,60 @@ float forward_backward(int *data, int len, int nstates, int nobvs, float *prior,
        //         xi[IDXT(j,k,nstates)] = logadd(xi[IDXT(j,k,nstates)], e);
        //     }
        // }
-        for (int j = 0; j < nstates; j+=8) {
+        for (int j = 0; j < nstates; j+=16) {
             __m256 gmm_AVX = _mm256_load_ps(gmm + data[len-i]*nstates + j);
+            __m256 gmm_AVX2 = _mm256_load_ps(gmm + data[len-i]*nstates + j + 8);
             __m256 alpha_AVX = _mm256_load_ps(alpha + (len-i) * nstates + j);
+            __m256 alpha_AVX2 = _mm256_load_ps(alpha + (len-i) * nstates + j + 8);
             __m256 beta_AVX = _mm256_load_ps(beta + (len-i) * nstates + j);
+            __m256 beta_AVX2 = _mm256_load_ps(beta + (len-i) * nstates + j + 8);
             alpha_AVX = _mm256_add_ps(alpha_AVX, beta_AVX);
+            alpha_AVX2 = _mm256_add_ps(alpha_AVX2, beta_AVX2);
             alpha_AVX = _mm256_sub_ps(alpha_AVX, loglik_AVX);
+            alpha_AVX2 = _mm256_sub_ps(alpha_AVX2, loglik_AVX);
             gmm_AVX = logadd(gmm_AVX, alpha_AVX);
+            gmm_AVX2 = logadd(gmm_AVX2, alpha_AVX2);
             _mm256_store_ps(gmm + data[len-i] * nstates + j, gmm_AVX);
+            _mm256_store_ps(gmm + data[len-i] * nstates + j + 8, gmm_AVX2);
 
             beta_AVX = _mm256_set1_ps(-INFINITY);
+            beta_AVX2 = _mm256_set1_ps(-INFINITY);
             alpha_AVX = _mm256_load_ps(alpha + (len-1-i) * nstates + j);
+            alpha_AVX2 = _mm256_load_ps(alpha + (len-1-i) * nstates + j + 8);
             for (int k = 0; k < nstates; k++) {
                 __m256 p_AVX = _mm256_set1_ps(beta[(len-i) * nstates + k]);
                 __m256 trans_AVX = _mm256_load_ps(transT + k * nstates + j);
+                __m256 trans_AVX2 = _mm256_load_ps(transT + k * nstates + j + 8);
                 __m256 obvs_AVX = _mm256_set1_ps(obvs[data[len-i] * nstates + k]);
-                p_AVX = _mm256_add_ps(p_AVX, trans_AVX);
-                p_AVX = _mm256_add_ps(p_AVX, obvs_AVX);
-                beta_AVX = logadd(beta_AVX, p_AVX); 
+                __m256 p_AVX1 = _mm256_add_ps(p_AVX, trans_AVX);
+                __m256 p_AVX2 = _mm256_add_ps(p_AVX, trans_AVX2);
+                p_AVX1 = _mm256_add_ps(p_AVX1, obvs_AVX);
+                p_AVX2 = _mm256_add_ps(p_AVX2, obvs_AVX);
+                beta_AVX = logadd(beta_AVX, p_AVX1); 
+                beta_AVX2 = logadd(beta_AVX2, p_AVX2); 
 
                 __m256 xi_AVX = _mm256_load_ps(xi + k*nstates + j);
+                __m256 xi_AVX2 = _mm256_load_ps(xi + k*nstates + j + 8);
                 __m256 e_AVX = _mm256_set1_ps(beta[(len-i) * nstates + k]);
+                __m256 e_AVX2 = _mm256_set1_ps(beta[(len-i) * nstates + k]);
                 // Use old transittion
                 // Use old obvs
                 e_AVX = _mm256_add_ps(e_AVX, alpha_AVX);
+                e_AVX2 = _mm256_add_ps(e_AVX2, alpha_AVX2);
                 e_AVX = _mm256_add_ps(e_AVX, trans_AVX);
+                e_AVX2 = _mm256_add_ps(e_AVX2, trans_AVX2);
                 e_AVX = _mm256_add_ps(e_AVX, obvs_AVX);
+                e_AVX2 = _mm256_add_ps(e_AVX2, obvs_AVX);
                 e_AVX = _mm256_sub_ps(e_AVX, loglik_AVX);
+                e_AVX2 = _mm256_sub_ps(e_AVX2, loglik_AVX);
                 xi_AVX = logadd(xi_AVX, e_AVX);
+                xi_AVX2 = logadd(xi_AVX2, e_AVX2);
                 _mm256_store_ps(xi + k*nstates + j, xi_AVX);
+                _mm256_store_ps(xi + k*nstates + j + 8, xi_AVX2);
             }
             // Store beta and xi
             _mm256_store_ps(beta + (len-1-i) * nstates + j, beta_AVX);
+            _mm256_store_ps(beta + (len-1-i) * nstates + j + 8, beta_AVX2);
 
         }
         //Store back to gmm
